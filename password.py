1e55220ac9d47fe5d2f1ae259d226d8d2d23b616
Pankil Panchal <panch.pankil99@gmail.com>
2019-04-17 00:32:28 +0530

    otp resolve

--- password.py
+++ password.py
@@ -1,20 +1,20 @@
 from cryptography.fernet import Fernet
 
 def encrypt(password):
-    key = Fernet.generate_key() #this is your "password"
-    cipher_suite = Fernet(key)
-    encoded_text = cipher_suite.encrypt(bytes(password, "utf-8"))
+    # key = Fernet.generate_key() #this is your "password"
+    # cipher_suite = Fernet(key)
+    # encoded_text = cipher_suite.encrypt(bytes(password, "utf-8"))
     output = []
-    output.append(str(encoded_text).split("'")[1])
-    output.append(str(key).split("'")[1])
+    output.append(str(password))
+    output.append(str('x'))
     return output
 
 def decrypt(password, key):
-    key = bytes(key, "utf-8")
-    cipher_suite = Fernet(key)
-    password = bytes(password, "utf-8")
-    decoded_text = cipher_suite.decrypt(password)
-    return (str(decoded_text).split("'")[1])
+    # key = bytes(key, "utf-8")
+    # cipher_suite = Fernet(key)
+    # password = bytes(password, "utf-8")
+    # decoded_text = cipher_suite.decrypt(password)
+    return (str(password))
 
 # x=encrypt("123456")
 
