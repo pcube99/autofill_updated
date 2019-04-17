dd312ff42af4fe99113270c7344956d6ac52c9be
Pankil Panchal <panch.pankil99@gmail.com>
2019-04-17 18:09:48 +0530

    full working code

--- js/popup.js
+++ js/popup.js
@@ -9,8 +9,6 @@ var boolpassword = false;
 var istransactioncomplete=true;
 setTimeout(function(){
   chrome.storage.local.get(['islogin'], function(result) {
-
-    console.log(result.islogin);
     if(result.islogin){
       document.getElementById('container').style.display = "none";
       document.getElementById('logined').style.display = "block";
@@ -52,29 +50,18 @@ async function fill(data, data1,xx ){
     
 
     if(data[yy]['area-label'].includes(data1[xx].toLowerCase()) || data[yy]['dname'].includes(data1[xx].toLowerCase()) || data[yy]['name'].includes(data1[xx].toLowerCase())){
-      // console.log(data1[i])
-
        if(flag[yy]==0){ 
          flag[yy] = 1;
-         console.log(data);
-         console.log(data1[xx].toLowerCase());
          await timer(500);
          var dta;
       
         chrome.storage.local.get(['login_response'],function(resultt){
         dta = JSON.parse(resultt.login_response)[xx][data1[xx]];
-        console.log(resultt.login_response);
-        
-        console.log(xx);
-        console.log(data1[xx]);
-        console.log(dta);
          chrome.storage.local.set({d1 : data[yy]['name']},function(){
            chrome.storage.local.set({d2: dta},function(){
              chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
                chrome.storage.local.get(['d1'],function(result1){
                  chrome.storage.local.get(['d2'],function(result2){
-                   console.log(xx + " " + result1.d1);
-                   console.log(xx + " " + result2.d2);
                    chrome.tabs.executeScript(
                        tabs[0].id,
                        {code: "document.getElementsByName('"+result1.d1+"')[0].value = '"+result2.d2+"';"});
@@ -118,11 +105,9 @@ function myAction(email, pass) {
   //login_response=response;
   chrome.storage.local.get(['login_response'],function(resultt){
     if(resultt.login_response){
-      console.log(resultt.login_response);
       //chrome.storage.local.get({islogin:}, function(result) {
         chrome.storage.local.set({islogin: true}, function(value) {
           chrome.storage.local.get(['islogin'], function(result) {
-           console.log(result);
             if(result.islogin){
           document.getElementById('container').style.display = "none";
           document.getElementById('logined').style.display = "block";
@@ -156,12 +141,7 @@ async function password_manager(){
       if(fill_website_email==data1[i]){
         var temp =i;
         boolemail=true;
-        console.log("ei is" + temp);
-        console.log(data1);
         chrome.storage.local.get(['login_response'],function(resultt){
-          console.log(JSON.parse(resultt.login_response));
-          //console.log("i isss "+i);
-          console.log(JSON.parse(resultt.login_response)[temp]);
         chrome.storage.local.set({data_website_email : JSON.parse(resultt.login_response)[temp][data1[temp]]},  function(){
           chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             chrome.storage.local.get(['data_website_email'],function(new_fill_website_email){
@@ -178,14 +158,8 @@ async function password_manager(){
     if(fill_website_password == data1[i]){
 
     var temp1 =i;
-    console.log("i isss "+temp1);
     boolpassword=true;
-      console.log("pi is" + temp1);
-      console.log(data1);
       chrome.storage.local.get(['login_response'],function(resultt){
-        console.log(JSON.parse(resultt.login_response));
-        // console.log("i isss "+temp1);
-        console.log(JSON.parse(resultt.login_response)[temp1]);
       chrome.storage.local.set({data_website_password : JSON.parse(resultt.login_response)[temp1][data1[temp1]]},  function(){
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
           chrome.storage.local.get(['data_website_password'],function(new_fill_website_password){
@@ -211,7 +185,6 @@ function documentEvents1() {
           url = tabs[0].url;
       });
      
-    console.log(url);
     httpGetAsync(("https://autofill-sen.herokuapp.com/autofill?url=" + url), function(response) {
   var ans=response;
   flag=[];
@@ -219,16 +192,12 @@ function documentEvents1() {
     data = JSON.parse(ans)[0];
     data1 = JSON.parse(ans)[1];
     count = data1.length;
-    console.log("count " + count);
-    console.log(data)
-    console.log(data1.length)
     for(var i =0;i<data1.length;i++){
       flag.push(0);
     }
     for(var j=0;j<data.length;j++){
       updateflag.push(0);
     }
-    console.log(flag);
     var i,j;
     for (var xx=0;xx<=data1.length;xx++){
       fill(data,data1,xx);
@@ -250,12 +219,9 @@ function documentEvents1() {
 function documentEvents2() {    
   document.getElementById('logout_btn').addEventListener('click', 
     function() { 
-      console.log("logouted");
       chrome.storage.local.set({islogin: false}, function(value) {
-        console.log('Value is set to ' + value);
         var islogin;
         chrome.storage.local.get(['islogin'], function(result) {
-         console.log(result);
           if(result.islogin){
         document.getElementById('container').style.display = "none";
         document.getElementById('logined').style.display = "block";
@@ -280,7 +246,6 @@ function myLoop (x) {
       chrome.storage.local.set({curr : data[ii]['name']},  function(){
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
           chrome.storage.local.get(['curr'],function(result1){
-              console.log(ii + " " + result1.curr);
               chrome.tabs.executeScript(
                   tabs[0].id,
                   {code: "var xmlHttp = new XMLHttpRequest(); xmlHttp.open('POST', 'https://autofill-sen.herokuapp.com/autoupdate?id=' + document.getElementsByName('"+result1.curr+"')[0].name.replace(/[^a-zA-Z ]/g, '') + '&value=' + document.getElementsByName('"+result1.curr+"')[0].value, false); xmlHttp.send(null);"});
@@ -309,7 +274,6 @@ function documentEvents3() {
       chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
         url = tabs[0].url;
     });
-    console.log(url);
     httpGetAsync(("https://autofill-sen.herokuapp.com/autofill?url=" + url), function(response) {
   var ans=response;
   flag=[];
@@ -317,20 +281,14 @@ function documentEvents3() {
     data = JSON.parse(ans)[0];
     data1 = JSON.parse(ans)[1];
     count = data1.length;
-    console.log("count " + count);
-    console.log(data)
-    console.log(data1.length)
     for(var i =0;i<data1.length;i++){
       flag.push(0);
     }
     for(var j=0;j<data.length;j++){
       updateflag.push(0);
     }
-    console.log(flag);
     });
-      console.log("autoupdate");
-      console.log(data);
-      console.log(data1);
+    
        //myLoop(data1.length);
        for(var ii=0;ii<data1.length;ii++){
        for(var j=0;j<data.length;j++){
@@ -343,20 +301,7 @@ function documentEvents3() {
         }
       }
     }
-      console.log(updateflag); 
       myLoop(data.length);
-    
-      // for(var i=0;i<10000000;i++)
-      // if(istransactioncomplete == true){break;}
-      
-    //   chrome.storage.local.get(['login_email'],function(result111){
-    //     chrome.storage.local.get(['login_password'],function(result112){
-    //   httpGetAsync(("https://autofill-sen.herokuapp.com/login?email=" + result111.login_email +"&password=" + result112.login_password), function(response) {
-    //     chrome.storage.local.set({login_response : response},function(){
-    //     });
-    //   });
-    // });
-    //   });
       httpGetAsync(("https://autofill-sen.herokuapp.com/autofill?url=" + url), function(response) {
         var ans=response;
         if(ans){
@@ -364,11 +309,8 @@ function documentEvents3() {
           data1 = JSON.parse(ans)[1];
         }
         });
-        console.log(data1);
-
         var website_url = url_convert(url);
         var website_password =url_convert(url) ;
-        console.log(website_password);
         chrome.storage.local.set({website_url : website_url},  function(){
         });
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -386,10 +328,7 @@ function documentEvents3() {
     });
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.storage.local.get(['website_password'],function(website){
-            console.log("xyzzzzzzzzzzzzzzzz");
-
-            console.log(website.website_password);
-            chrome.tabs.executeScript(
+                      chrome.tabs.executeScript(
                 tabs[0].id,
                 {code: "var xmlHttp = new XMLHttpRequest(); xmlHttp.open('POST', 'https://autofill-sen.herokuapp.com/autoupdate?id=password' +'"+ website.website_password+"'  + '&value=' + document.getElementsByName('password')[0].value, false); xmlHttp.send(null);"});
         
@@ -401,8 +340,6 @@ function documentEvents3() {
     chrome.storage.local.get(['login_password'],function(result112){
   httpGetAsync(("https://autofill-sen.herokuapp.com/login?email=" + result111.login_email +"&password=" + result112.login_password), function(response) {
     chrome.storage.local.set({login_response : response},function(){
-      console.log("pankil panchal");
-      console.log(response);
     });
   });
 
@@ -411,7 +348,6 @@ function documentEvents3() {
 });
  
          
-  console.log("out ");
   if(typeof url === "undefined")
   {
     document.getElementById("autoupdate_btn").click();
